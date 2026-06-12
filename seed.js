7becd21ba8e553e2869bf090727eddc711d99e7b
EdwinArmentaIndq <edwin.armenta@indqcorp.com>
2026-06-12 13:22:18 -0700

    arreglado la base de datos ya no se creara multiples veces

--- seed.js
+++ seed.js
@@ -28,135 +28,79 @@ function buscar(url){
 export async function seedDB(){
     //let flag=false
     try{
+        const countPlanets = await Planeta.countDocuments();
+        if(countPlanets===0){
+            console.log("RECOLECTANDO INFO DE PLANETAS...")
+            const data_planetas = await axios.get(process.env.API_PLANETAS)
+            console.log("PLANETAS OBTENIDOS ")
+            console.log("Insertando planetas")
+            await Planeta.insertMany(data_planetas.data);
+            console.log("planetas insertados correctamente ")
+        }
+       else{
+        console.log("Planetas ya registrados")
+       }
 
-        console.log("RECOLECTANDO INFO DE PLANETAS...")
-        const data_planetas = await axios.get(process.env.API_PLANETAS)
-        data_planetas.data.forEach(planeta=>{
-            const aux = extraerID(planeta.url);
-            planeta.api_id = aux[aux.length-1];
-        })
-        console.log("PLANETAS OBTENIDOS ")
-        console.log("Insertando planetas")
-        await Planeta.insertMany(data_planetas.data);
-        console.log("planetas insertados correctamente ")
-
-
+       const countStarships = await Nave.countDocuments();
+       if(countStarships===0){
         console.log("RECOLECTANDO INFO DE NAVES...")
         const data_naves = await axios.get(process.env.API_NAVES)
-        data_naves.data.forEach(nave=>{
-            const aux = extraerID(nave.url);
-            nave.api_id = aux[aux.length-1];
-        })
         console.log("NAVES OBTENIDAS ")
         console.log("Insertando naves")
         await Nave.insertMany(data_naves.data);
         console.log("naves insertadas correctamente ")
 
+       }
+       else{
+        console.log("Naves ya registradas")
+       }
 
+       const countMovies = await Pelicula.countDocuments();
+       if(countMovies === 0){
         console.log("RECOLECTANDO INFO DE PELICULAS...")
         const data_peliculas = await axios.get(process.env.API_PELICULAS)
-          data_peliculas.data.forEach(peli=>{
-            const aux = extraerID(peli.url);
-            peli.api_id = aux[aux.length-1];
-        })
         console.log("PELICULAS OBTENIDA... ")
         console.log("Insertando peliculas")
         await Pelicula.insertMany(data_peliculas.data);
         console.log("Peliculas insertadas correctamente ")
+       }
+       else{
+        console.log("Películas ya registradas")
+       }
 
+       const countVehicles = await Vehiculo.countDocuments();
+       if(countVehicles === 0){ 
+            console.log("RECOLECTANDO INFO DE VEHICULOS...")
+            const data_vehiculos = await axios.get(process.env.API_VEHICULOS)
+            console.log("VEHICULOS OBTENIDOS ")
+            console.log("Insertando vehiculos")
+            await Vehiculo.insertMany(data_vehiculos.data);
+            console.log("vehiculos insertados correctamente ")
+       } 
+       else{
+        console.log("Vehículos ya registrados")
+       }
 
-        console.log("RECOLECTANDO INFO DE VEHICULOS...")
-        const data_vehiculos = await axios.get(process.env.API_VEHICULOS)
-        data_vehiculos.data.forEach(vehiculo=>{
-            const aux = extraerID(vehiculo.url);
-            vehiculo.api_id = aux[aux.length-1];
-        })
-        console.log("VEHICULOS OBTENIDOS ")
-        console.log("Insertando vehiculos")
-        await Vehiculo.insertMany(data_vehiculos.data);
-        console.log("vehiculos insertados correctamente ")
-
-        console.log("RECOLECTANDO INFO DE ESPECIES...")
-        const data_especies = await axios.get(process.env.API_ESPECIES)
-        console.log("ESPECIES OBTENIDAS ")
-        relacionarEspecies(data_especies.data);
-
-
-       /* console.log("Insertando especies")
-        await Especie.insertMany(data_especies.data);
-        console.log("Especies insertadas correctamente ")*/
-
-        console.log("RECOLECTANDO INFO DE PERSONAJES...")
-        const data_personajes = await axios.get(process.env.API_PERSONAJES)
-        console.log("PERSONAJES OBTENIDOS... ")
-        relacionarPersonajes(data_personajes.data)
-       /* const data_personajes_promise = await Promise.all(
-            data_personajes.data.map(async (personaje)=>{
-            //const aux= extraerID(personaje.homeworld);
-            //personaje.homeworld = planetasDict[personaje.homeworld]
-            const planeta_origen = await axios.get(personaje.url);
-            const planeta_db = await Planeta.find({name: planeta_origen.name})
-            personaje.homeworld=planeta_db._id
-
-            if(personaje.vehicles.length>0 || personaje.vehicles !== null){
-               let v=[]
-               const vehiculos_promise = await Promise.all(
-                personaje.vehicles.map(async (vehiculo) =>{
-                const vehiculo_propio = await axios.get(vehiculo)
-                const vehiculo_db = await Vehiculo.find({name: vehiculo_propio.name})
-                v.push(vehiculo_db._id)
-            }))
-                
-            personaje.vehicles = v;
-        }
-
-            if(personaje.starships.length>0){
-                let s=[]
-                const starships_promise = await Promise.all(
-                    personaje.starships.map(async (nave)=>{
-                    const nave_propia =await axios.get(nave)
-                    const nave_DB = await Nave.find({name: nave_propia.name})
-                    s.push(nave_DB._id)
-                })
-                )
-                personaje.starships=s;
-            }
-            
-
-            if(personaje.films.length>0){
-                let f=[]
-                const films_promise = await Promise.all(
-                    personaje.films.map(async (peli) =>{
-                    const peli_propia = await axios.get(peli)
-                    const peli_DB = await Pelicula.find({title:peli_propia.name})
-                    f.push(peli_DB._id)
-                }))
-                personaje.films=f;
-            }
-            
- 
-            if(personaje.species.lenght>0){
-                let sp=[]
-                const species_promise = await Promise.all(
-                    personaje.species.map(async (espe) => {
-                    const espe_propia = await axios.get(espe)
-                    const espe_DB = await Especie.find({name: espe_propia.name})
-                    console.log("id especie ", espe_DB._id)
-                    sp.push(espe_DB._id)
-                }))
-                personaje.species=sp;
-                console.log(personaje.species)
-            }
-            
-        })
-        )
-        
-        console.log("Insertando personajes")
-        console.log(data_personajes.data)
-        await Personaje.insertMany(data_personajes.data);
-        console.log("Personajes insertados correctamente ")*/
-
-
+       const countSpecies = await Especie.countDocuments();
+       if(countSpecies === 0){
+            console.log("RECOLECTANDO INFO DE ESPECIES...")
+            const data_especies = await axios.get(process.env.API_ESPECIES)
+            console.log("ESPECIES OBTENIDAS ")
+            relacionarEspecies(data_especies.data);
+       }else{
+        console.log("Especies ya registradas")
+       }
+       
+       const countCharacters = await Personaje.countDocuments();
+       if(countCharacters === 0){
+            console.log("RECOLECTANDO INFO DE PERSONAJES...")
+            const data_personajes = await axios.get(process.env.API_PERSONAJES)
+            console.log("PERSONAJES OBTENIDOS... ")
+            relacionarPersonajes(data_personajes.data)   
+       }else{
+        console.log("Personajes ya registrados")
+       }
+           
        console.log("Todo registrado")
     } catch (error){
         console.error("Fallo al insertar ", error);
